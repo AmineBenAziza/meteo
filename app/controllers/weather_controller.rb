f329f911e830d6ddeb9a4acb99cb94b5a5bd87ff
AmineBenAziza <benazizalamine1@gmail.com>
2023-06-29 03:58:02 +0200

    FEATURE select for many days
    Details
    ------------------
    add my city weather's page
    
    Others
    ------------------

--- app/controllers/weather_controller.rb
+++ app/controllers/weather_controller.rb
@@ -2,10 +2,14 @@ class WeatherController < ApplicationController
   def search
     if params[:city_id]
       city = City.find(params[:city_id])
-      data = $client.current_weather(city: city.name, dt: Time.current.tomorrow)
+      dt = params[:date].present? ? params[:date] : Time.current.tomorrow
+      data = $client.current_weather(city: city.name, dt: dt)
       @temp = data.main.temp_c
-
-      redirect_to root_path(temp: @temp)
+      if params[:date].present?
+        redirect_to home_my_weather_path(temp: @temp, dt: dt, city: city.name)
+      else
+        redirect_to root_path(temp: @temp, city: city.name )
+      end
     else
       flash['error'] = 'city dos not exist'
     end
